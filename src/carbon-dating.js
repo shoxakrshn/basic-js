8c7fa38e858ccdc50af3cce77b0a701753b413a3
shoxa <sllx.ok@gmail.com>
2023-04-12 22:41:04 +0500

    feat: Carbon-Dating - done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,30 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+
+  if (isNaN(sampleActivity)) {
+    return false;
+  }
+
+  if (arguments.length === 0) {
+    return false;
+  }
+
+  if ( +sampleActivity <= 0) {
+    return false;
+  }
+
+  if (sampleActivity > MODERN_ACTIVITY) {
+    return false;
+  }
+
+  let k = 0.693 / HALF_LIFE_PERIOD;
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / k);
 }
 
 module.exports = {
