6bc0b75c8afb4f2a2ad88ac296e68c6db7b120ff
shoxa <sllx.ok@gmail.com>
2023-04-13 15:57:38 +0500

    feat: Extended-Reparater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const initial = { separator: '+', additionSeparator: '|' };
+  options = {...initial, ...options};
+  const main = [];
+  const add = [];
+  let word = str;
+
+  if (typeof str !== 'string' && typeof options.addition !== 'string') {
+    word = String(str);
+    options.addition = String(options.addition)
+  }
+
+  if (!options.repeatTimes && !options.additionRepeatTimes) {
+    return str + options.addition;
+  }
+
+  if (options.addition && !options.additionRepeatTimes) {
+    word =  str + options.addition;
+  }
+
+  for (let i = 0; i < options.repeatTimes; i += 1) {
+    main.push(word);
+  }
+  
+  for (let j = 0; j < options.additionRepeatTimes; j += 1) {
+    add.push(options.addition)
+  }
+  
+  const addReady = add.join(options.additionSeparator)
+  const mainReady = main.map((item) => item += addReady);
+  
+  return mainReady.join(options.separator);
 }
+
+
+
+  
 
 module.exports = {
   repeater
