bf65120538d2553543a2bc3ea786bcb894f87ad2
shoxa <sllx.ok@gmail.com>
2023-04-14 21:33:54 +0500

    feat: Vigenere-Cipher Done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,72 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(str, key) {
+    if (!str || !key ) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    if (!key || key === undefined) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    const strUp = str.toUpperCase();
+    const keyUp = key.toUpperCase();
+
+    let ciphered = '';
+    let keyIdx = 0;
+
+    for (let i = 0; i < strUp.length; i += 1) {
+
+      if (this.alphabet.includes(strUp[i])) {
+        const strIndex = this.alphabet.indexOf(strUp[i]);
+        const keyIndexMod = this.alphabet.indexOf(keyUp[keyIdx % key.length]);
+        const cipheredLetter = this.alphabet[(strIndex + keyIndexMod) % 26];
+        ciphered += cipheredLetter;
+        keyIdx += 1;
+      } else {
+      ciphered += strUp[i];
+      }
+    }
+
+    return this.type ? ciphered : ciphered.split('').reverse().join('');
+  }
+
+  decrypt(cipher, key) {
+    if (!cipher || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    const cipherUp = cipher.toUpperCase();
+    const keyUp = key.toUpperCase();
+
+    let deciphered = '';
+    let keyIdx = 0;
+
+    for (let i = 0; i < cipherUp.length; i += 1) {
+      if (this.alphabet.includes(cipherUp[i])) {
+        const cipherIdx = this.alphabet.indexOf(cipherUp[i]);
+        const keyIndexMod = this.alphabet.indexOf(keyUp[keyIdx % key.length]);
+        const isPositive = (cipherIdx - keyIndexMod);
+        const decipheredLetterIdx = isPositive >= 0 ? isPositive % 26 : (isPositive + 26) % 26;
+        const decipheredLetter = this.alphabet[decipheredLetterIdx];
+        
+        deciphered += decipheredLetter;
+        keyIdx += 1;
+      } else {
+        deciphered += cipherUp[i];
+      }
+    }
+
+    return this.type ? deciphered : deciphered.split('').reverse().join('');
   }
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+
